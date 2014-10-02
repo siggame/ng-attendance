537b3e678b2fea2626cd8c7f310a9ac7e3f13130
jswv9@mst.edu <jswv9@mst.edu>
2014-10-01 21:27:05 -0500

    Add names list template and some backend

---
+++ names.py
@@ -0,0 +1,2 @@
+def names_data():
+  return
--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from flask import render_template
-from flash import request
+from flask import request
+
+import argparse
+import names
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template("name_list.html")
+  return render_template("names_list.html")
+
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("names",
+                      type=file,
+                      help="JSON file with a list of names")
+  args = parser.parse_args()
+
+  with args.names as names_file:
+    names = json.load(names_file)
+    names_list = list(names)
+
   app.run(debug=True)
